fa7765a29a74b86f3650e9a3df3c0499d08c2293
alice T <alicewtien@gmail.com>
2018-09-16 18:37:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{seasons}:".to_s.capitalize!
     holidays_in_season.each do |holiday, supplies|
       if holiday.to_s.split("_").size > 1
-        holiday = holiday.to_s.split("_")
-        puts "  #{holiday.map(&:capitalize).join(" ")}: #{supplies.join(", ")}"
+        holiday = holiday.to_s.split("_").each {|word| word.capitalize!}
+        puts "  #{holiday.join(" ")}: #{supplies.join(", ")}"
       else
         puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
       end
