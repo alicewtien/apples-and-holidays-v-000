60329754b01a5c24ff18afe32d7ac1556384f0db
alice T <alicewtien@gmail.com>
2018-09-16 18:26:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{seasons}:".to_s.capitalize!
     holidays_in_season.each do |holiday, supplies|
       if holiday.to_s.split("_").size > 1
-        holiday = holiday.to_s.split("_").to_s.capitalize.join(" ")
-        puts "  #{holiday}: #{supplies.join(", ")}"
+        holiday = holiday.to_s.split("_").to_s.capitalize
+        puts "  #{holiday.to_s.join(" ")}: #{supplies.join(", ")}"
       else
         puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
       end
